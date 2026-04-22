8be266f5a1caeed53fb6b38dc9d3f193da16112f
proy <proyek1perpustakaa@gmail.com>
2026-04-22 13:13:38 +0700

    pe bisa

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,3 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+export default defineConfig({});
